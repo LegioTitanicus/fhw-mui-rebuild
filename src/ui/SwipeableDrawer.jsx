7954a8be98dff29b863af8d9586057b20d93da7c
LegioTitanicus <alexbernstein7170@gmail.com>
2020-06-20 20:24:03 -0400

    Co-authored-by: thenextfreud <thenextfreud@gmail.com>

--- src/ui/SwipeableDrawer.jsx
+++ src/ui/SwipeableDrawer.jsx
@@ -1,24 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+import theme from "./theme";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  //   appBar: {
+  //     zIndex: theme.zIndex.drawer + 1,
+  //   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  //   toolbar: theme.mixins.toolbar,
+  //   drawerPaper: {
+  //     width: drawerWidth,
+  //   },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  closeMenuButton: {
+    marginRight: "auto",
+    marginLeft: 0,
+  },
+}));
 
 const SwipeableDrawer = (props) => {
-  const [mobileOpen, setMobileOpen] = React.useState(false);
-  handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+  const classes = useStyles();
+  //   const [mobileOpen, setMobileOpen] = useState(false);
+  //   const handleDrawerToggle = () => {
+  //     console.log(mobileOpen);
+  //     setMobileOpen(!mobileOpen);
+  //   };
 
   const drawer = (
     <div>
       <List>
-        {props.pages.map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemText primary={text} />
+        {props.pages.map((page) => (
+          <ListItem button key={page.key}>
+            <ListItemText primary={page.title} />
           </ListItem>
         ))}
       </List>
     </div>
   );
 
-  return <div></div>;
+  return (
+    <nav className={classes.drawer}>
+      {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+      <Hidden smUp implementation="css">
+        <Drawer
+          variant="temporary"
+          anchor={theme.direction === "rtl" ? "right" : "left"}
+          open={props.mobileOpen}
+          onClose={props.handleDrawerToggle}
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+        >
+          <IconButton
+            onClick={props.handleDrawerToggle}
+            className={classes.closeMenuButton}
+          >
+            <CloseIcon />
+          </IconButton>
+          {drawer}
+        </Drawer>
+      </Hidden>
+    </nav>
+  );
 };
 
 export default SwipeableDrawer;
--- src/ui/Topnav.jsx
+++ src/ui/Topnav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -6,9 +6,14 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import CenteredTabs from "./CenteredTabs";
 import Box from "@material-ui/core/Box";
 import { Grid } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import Hidden from "@material-ui/core/Hidden";
+
+import CenteredTabs from "./CenteredTabs";
+import SwipeableDrawer from "./SwipeableDrawer";
+import theme from "./theme";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,6 +29,9 @@ const useStyles = makeStyles((theme) => ({
 
 const Topnav = () => {
   const classes = useStyles();
+
+  const isSmall = useMediaQuery(theme.breakpoints.up("sm"));
+
   const pages = [
     { key: 1, title: "Home" },
     { key: 2, title: "About" },
@@ -32,21 +40,37 @@ const Topnav = () => {
     { key: 5, title: "Plasma" },
   ];
 
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const handleDrawerToggle = () => {
+    console.log("toggle menu");
+    setMobileOpen(!mobileOpen);
+  };
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Grid container justify="center">
-            <CenteredTabs pages={pages} />
-          </Grid>
+          {isSmall ? (
+            <Grid container justify="center">
+              <CenteredTabs pages={pages} />
+            </Grid>
+          ) :
+             <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+          )
+          <Hidden smUp>
+            <SwipeableDrawer
+              pages={pages}
+              handleDrawerToggle={handleDrawerToggle}
+            />
+          </Hidden>
         </Toolbar>
       </AppBar>
     </div>
